2c1a1b8449d6c90abfaa715ea8f560844a9683c7
Nurprynce <d_alexander_18@outlook.com>
2025-07-16 16:44:19 -0500

    Corrijo: agrego el código que faltaba en Proyecto A y Proyecto B

---
+++ Proyecto_A/Ingreso_Datos.py
@@ -0,0 +1,8 @@
+def Metodo_LeerA():
+    return float(input("Ingrese el primer número: "))
+
+def Metodo_LeerB():
+    return float(input("Ingrese el segundo número: "))
+
+def Metodo_Leer_a():
+    return float(input("Ingrese un número para operación avanzada: "))
