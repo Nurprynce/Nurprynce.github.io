386cc88dfc3ac401345839689df52bf5982ff74c
Nurprynce <d_alexander_18@outlook.com>
2025-07-16 16:38:58 -0500

    Corrijo y actualizo el código de main.py

---
+++ Proyecto_B/main.py
@@ -0,0 +1,23 @@
+from Titulares import Titulos
+from Accesos import Ingreso_Datos
+from Basicas import Operaciones_Basicas
+from Avanzados import Operaciones_Avanzadas
+
+def main():
+    Titulos.mostrar_titulo("Operaciones Básicas")
+    a = Ingreso_Datos.Metodo_LeerA()
+    b = Ingreso_Datos.Metodo_LeerB()
+    
+    print(f"Suma: {Operaciones_Basicas.suma(a, b)}")
+    print(f"Resta: {Operaciones_Basicas.resta(a, b)}")
+    print(f"Multiplicación: {Operaciones_Basicas.multiplicacion(a, b)}")
+    print(f"División: {Operaciones_Basicas.division(a, b)}")
+    
+    Titulos.mostrar_titulo("Operaciones Avanzadas")
+    x = Ingreso_Datos.Metodo_Leer_a()
+    
+    print(f"Cuadrado: {Operaciones_Avanzadas.cuadrado(x)}")
+    print(f"Cubo: {Operaciones_Avanzadas.cubo(x)}")
+
+if __name__ == "__main__":
+    main()
